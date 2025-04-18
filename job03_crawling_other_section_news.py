593ddba03a16c5922c0a07650130d5977b5de5e1
hyunsoo-Oh <hyunsoo2245@gmail.com>
2025-04-18 10:41:20 +0900

    job03_crawling

--- job03_crawling_other_section_news.py
+++ job03_crawling_other_section_news.py
@@ -27,28 +27,33 @@ for section_id in range(6):
     driver.get(url)  # 5초 동안 Browser 띄우기
     button_xpath = '//*[@id="newsct"]/div[{}]/div/div[2]'.format(5 if section_id == 1 else 4)
 
-    # 더보기 버튼 클릭 (최대 15번)
-    cnt = [0, 0, 0, 0, 0, 0];
-    for i in range(200): # 6개의 컨테이너
+    # 더보기 버튼 클릭
+    click_counts = [0] * 6  # 각 섹션별 클릭 횟수 저장
+    for _ in range(15):  # 최대 200번 시도
         try:
             time.sleep(0.5)
             driver.find_element(By.XPATH, button_xpath).click()
-            cnt[section_id] += 1
+            click_counts[section_id] += 1
         except:
             break
     time.sleep(2)
     # 기사 수집 : container 1칸에 6개 titles
     titles = []
-    for i in range(1, cnt[section_id]+1):  # container
-        for j in range(1, 7):  # title
+    total_articles = click_counts[section_id] * 6  # 한 번 클릭당 6개 기사 추가됨
+
+    # 각 기사 수집
+    for i in range(1, total_articles):
+        for j in range(1, 7):  # 컨테이너 내 위치 (1~6)
             s = 4 if section_id != 1 else 5
-            title_path = '//*[@id="newsct"]/div[{}]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(s, i, j)
+            title_xpath = '//*[@id="newsct"]/div[{}]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(s, i, j)
             try:
-                title = driver.find_element(By.XPATH, title_path).text
+                title = driver.find_element(By.XPATH, title_xpath).text
                 titles.append(title)
                 print(f"[{categories[section_id]}] {title}")
             except:
                 print('error', i, j)
+                if i != 3:
+                    break
     df_section_titles = pd.DataFrame(titles, columns=['titles'])
     df_section_titles['category'] = categories[section_id]
     df_titles = pd.concat([df_titles, df_section_titles],
